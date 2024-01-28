91033d3d9bcf543d59dd11147c89282abaac9e2d
dsol-cpu <dmsolinsky@email.wm.edu>
2024-01-28 17:22:45 -0500

    go

--- scripts/victory_screen.gd
+++ scripts/victory_screen.gd
@@ -2,13 +2,13 @@ extends Control
 class_name VictoryScreen
 
 @export var game_music : NodePath
-@onready var game_music_node: AudioStreamPlayer
+@onready var game_music_node: AudioStreamPlayer = get_node(game_music)
 
 @export var victory_sound : NodePath
 @onready var victory_sound_node : AudioStreamPlayer3D = get_node(victory_sound)
 
 func victory_yay() -> void:
 	game_music_node._set_playing(false)
+	victory_sound_node._set_playing(true)
 	set_visible(true)
 	Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
-	victory_sound_node._set_playing(true)
